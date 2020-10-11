5f1f98f1454a4b6941cf2ea21ef0678c6e4602fa
Sergio Ruy <sergio_ruy@hotmail.com>
2020-10-11 10:53:31 -0300

    Cross Origin annotation added to controller.

--- src/main/java/sergioruy/employeelistapi/controller/EmployeeController.java
+++ src/main/java/sergioruy/employeelistapi/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package sergioruy.employeelistapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import sergioruy.employeelistapi.repository.EmployeeRepository;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
