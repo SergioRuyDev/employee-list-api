4c80fe527fd7ddfea255ecae6f86b850b123dfd2
Sergio Ruy <sergio_ruy@hotmail.com>
2020-10-11 07:36:44 -0300

    Initial commit

---
+++ src/main/java/sergioruy/employeelistapi/EmployeeListApiApplication.java
@@ -0,0 +1,13 @@
+package sergioruy.employeelistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeListApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeListApiApplication.class, args);
+    }
+
+}
