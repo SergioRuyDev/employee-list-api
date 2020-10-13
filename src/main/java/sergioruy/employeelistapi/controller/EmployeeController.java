e4a181a77fa52fa1bbf145dbd3a319374bdd9fb3
Sergio Ruy <sergio_ruy@hotmail.com>
2020-10-13 06:57:52 -0300

    Endpoint Update(Put) added.

--- src/main/java/sergioruy/employeelistapi/controller/EmployeeController.java
+++ src/main/java/sergioruy/employeelistapi/controller/EmployeeController.java
@@ -36,4 +36,18 @@ public class EmployeeController {
                 .orElseThrow(() -> new ResourceNotFoundException("Employ not exist with id : " + id));
         return ResponseEntity.ok(employee);
     }
+
+    // update the employee
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employ not exist with id : " + id));
+
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailId(employeeDetails.getEmailId());
+
+        Employee updateEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updateEmployee);
+    }
 }
