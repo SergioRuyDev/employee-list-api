c2710eaccf2bbd9e54ca8703653582fe03b95732
sergioruydev <sergioruyenator@gmail.com>
2022-02-10 15:21:49 -0300

    Repository tested.

--- src/main/java/sergioruy/employeelistapi/repository/EmployeeRepository.java
+++ src/main/java/sergioruy/employeelistapi/repository/EmployeeRepository.java
@@ -8,7 +8,6 @@ import sergioruy.employeelistapi.model.Employee;
 
 import java.util.Optional;
 
-@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     Optional<Employee> findByEmailId(String emailId);
@@ -20,6 +19,15 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     // define custom query using JPQL with named params
     @Query("select e from Employee e where e.firstName =:firstName and e.lastName =:lastName")
     Employee findByJPQLNamedParams(@Param("firstName") String firstName, @Param("lastName") String lastName);
+
+    // define custom query using native SQL with index params
+    @Query(value = "select * from employees e where e.first_name =?1 and e.last_name =?2", nativeQuery = true)
+    Employee findByNativeSQL(String firstName, String lastName);
+
+    // define custom query using native SQL with named params
+    @Query(value = "select * from employees e where e.first_name =:firstName and e.last_name =:lastName", nativeQuery = true)
+    Employee findByNativeSQLNamed(@Param("firstName") String firstName, @Param("lastName") String lastName);
+
 
 
 
