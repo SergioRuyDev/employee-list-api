e355ddd30a521ed2f17a958906cca1bc5fff97a5
SergioRuyDev <sergioruyenator@gmail.com>
2022-02-11 15:21:48 -0300

    Tested save method with throws exception.

--- src/test/java/sergioruy/employeelistapi/service/EmployeeServiceTest.java
+++ src/test/java/sergioruy/employeelistapi/service/EmployeeServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import sergioruy.employeelistapi.exception.ResourceNotFoundException;
 import sergioruy.employeelistapi.model.Employee;
 import sergioruy.employeelistapi.repository.EmployeeRepository;
 import sergioruy.employeelistapi.service.impl.EmployeeServiceImpl;
@@ -14,7 +15,11 @@ import sergioruy.employeelistapi.service.impl.EmployeeServiceImpl;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class EmployeeServiceTest {
@@ -55,6 +60,26 @@ class EmployeeServiceTest {
         System.out.println(savedEmployee); // just for make sure that are not null
         //then
         assertThat(savedEmployee).isNotNull();
+
+    }
+
+    //JUnit test for saveEmployee method with throws exception
+    @DisplayName("JUnit test for saveEmployee method with throws exception")
+    @Test
+    public void givenEmployeeObject_whenSaveEmployee_thenThrowsException() {
+        //given
+        given(employeeRepository.findByEmailId(employee.getEmailId())).willReturn(Optional.of(employee));
+
+        System.out.println(employeeRepository); // make sure is mocked
+        System.out.println(employeeService); // make sure about is mocked
+
+        //when
+        assertThrows(ResourceNotFoundException.class, () -> {
+            employeeService.saveEmployee(employee);
+        });
+
+        //then
+        verify(employeeRepository, never()).save(any(Employee.class));
 
     }
 }
