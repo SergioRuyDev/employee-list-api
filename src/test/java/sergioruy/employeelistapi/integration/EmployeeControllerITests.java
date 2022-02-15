6044855f0915128bc6bcc2f720756e6e79e03e7e
SergioRuyDev <sergioruyenator@gmail.com>
2022-02-15 15:57:16 -0300

    Integration tests in progress.

--- src/test/java/sergioruy/employeelistapi/integration/EmployeeControllerITests.java
+++ src/test/java/sergioruy/employeelistapi/integration/EmployeeControllerITests.java
@@ -1,17 +1,17 @@
-package sergioruy.employeelistapi.controller;
+package sergioruy.employeelistapi.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import sergioruy.employeelistapi.model.Employee;
-import sergioruy.employeelistapi.service.EmployeeService;
+import sergioruy.employeelistapi.repository.EmployeeRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,26 +20,28 @@ import java.util.Optional;
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.willDoNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
-
-@WebMvcTest //only the spring beans required for test the controllers
-class EmployeeControllerTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class EmployeeControllerITests {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private EmployeeService employeeService;
-
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        employeeRepository.deleteAll();
+    }
 
 
     //JUnit test for getAll Employees
@@ -51,7 +53,7 @@ class EmployeeControllerTest {
         List<Employee> listOfEmployees = new ArrayList<>();
         listOfEmployees.add(Employee.builder().firstName("Sergio").lastName("Ruy").emailId("sergio@gmail.com").build());
         listOfEmployees.add(Employee.builder().firstName("Tony").lastName("Stark").emailId("tony@gmail.com").build());
-        given(employeeService.getAllEmployees()).willReturn(listOfEmployees); //stubbing call
+        employeeRepository.saveAll(listOfEmployees);
 
         //when - is the action or the behavior we are going to test
         ResultActions response = mockMvc.perform(get("/api/v1/employees"));
@@ -59,7 +61,7 @@ class EmployeeControllerTest {
         //then - verify the result
         response.andExpect(status().isOk())
                 .andDo(print())
-                .andExpect(jsonPath("$.size()", CoreMatchers.is(listOfEmployees.size())));
+                .andExpect(jsonPath("$.size()", is(listOfEmployees.size())));
 
     }
 
@@ -69,16 +71,15 @@ class EmployeeControllerTest {
     public void givenEmployeeId_whenGetEmployeeById_thenReturnEmployeeObject() throws Exception {
 
         //given - is a precondition or a setup
-        long employeeId = 1L;
         Employee employee = Employee.builder()
                 .firstName("Sergio Ruy")
                 .lastName("Ruy")
                 .emailId("sergio@gmail.com")
                 .build();
-        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.of(employee));
+        employeeRepository.save(employee);
 
         //when - is the action or the behavior we are going to test
-        ResultActions response = mockMvc.perform(get("/api/v1/employees/{id}", employeeId));
+        ResultActions response = mockMvc.perform(get("/api/v1/employees/{id}", employee.getId()));
 
         //then - verify the result
         response.andDo(print())
@@ -101,7 +102,7 @@ class EmployeeControllerTest {
                 .lastName("Ruy")
                 .emailId("sergio@gmail.com")
                 .build();
-        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.empty());
+        employeeRepository.save(employee);
 
         //when - is the action or the behavior we are going to test
         ResultActions response = mockMvc.perform(get("/api/v1/employees/{id}", employeeId));
@@ -122,8 +123,6 @@ class EmployeeControllerTest {
                 .lastName("Ruy")
                 .emailId("sergio@gmail.com")
                 .build();
-        given(employeeService.saveEmployee(any(Employee.class)))
-                .willAnswer((invocation) -> invocation.getArgument(0));
 
         // when
         ResultActions response = mockMvc.perform(post("/api/v1/employees")
@@ -144,12 +143,12 @@ class EmployeeControllerTest {
     public void givenUpdatedEmployee_whenUpdateEmployee_thenReturnUpdatedEmployeeObject() throws Exception {
 
         // given
-        long employeeId = 1L;
         Employee savedEmployee = Employee.builder()
                 .firstName("Sergio")
                 .lastName("Ruy")
                 .emailId("sergio@gmail.com")
                 .build();
+        employeeRepository.save(savedEmployee);
 
         Employee updatedEmployee = Employee.builder()
                 .firstName("Ruy")
@@ -157,12 +156,8 @@ class EmployeeControllerTest {
                 .emailId("junior@gmail.com")
                 .build();
 
-        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.of(savedEmployee));
-        given(employeeService.updateEmployee(any(Employee.class)))
-                .willAnswer((invocation) -> invocation.getArgument(0));
-
         // when
-        ResultActions response = mockMvc.perform(put("/api/v1/employees/{id}", employeeId)
+        ResultActions response = mockMvc.perform(put("/api/v1/employees/{id}", savedEmployee.getId())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(updatedEmployee)));
 
@@ -172,84 +167,5 @@ class EmployeeControllerTest {
                 .andExpect(jsonPath("$.firstName", is(updatedEmployee.getFirstName())))
                 .andExpect(jsonPath("$.lastName", is(updatedEmployee.getLastName())))
                 .andExpect(jsonPath("$.emailId", is(updatedEmployee.getEmailId())));
-    }
-
-    //JUnit test of method update operation - negative scenario
-    @DisplayName("JUnit test of update operation - negative scenario")
-    @Test
-    public void givenUpdatedEmployee_whenUpdateEmployee_thenReturnStatusNotFound() throws Exception {
-
-        // given
-        long employeeId = 1L;
-        Employee savedEmployee = Employee.builder()
-                .firstName("Sergio")
-                .lastName("Ruy")
-                .emailId("sergio@gmail.com")
-                .build();
-
-        Employee updatedEmployee = Employee.builder()
-                .firstName("Ruy")
-                .lastName("Junior")
-                .emailId("junior@gmail.com")
-                .build();
-
-        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.empty());
-        given(employeeService.updateEmployee(any(Employee.class)))
-                .willAnswer((invocation) -> invocation.getArgument(0));
-
-        // when
-        ResultActions response = mockMvc.perform(put("/api/v1/employees/{id}", employeeId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(updatedEmployee)));
-
-        // then
-        response.andDo(print()).
-                andExpect(status().isNotFound());
-    }
-
-    //Junit test for delete employee in Positive scenario
-    @DisplayName("Junit test for delete employee in Positive scenario")
-    @Test
-    public void givenEmployeeId_whenDeleteEmployee_thenReturnStatusOk() throws Exception {
-
-        //given - is a precondition or a setup
-        long employeeId = 1L;
-        Employee savedEmployee = Employee.builder()
-                .firstName("Sergio")
-                .lastName("Ruy")
-                .emailId("sergio@gmail.com")
-                .build();
-        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.of(savedEmployee));
-        willDoNothing().given(employeeService).deleteEmployee(employeeId); //will do nothing because the method is void
-
-        //when - is the action or the behavior we are going to test
-        ResultActions response = mockMvc.perform(delete("/api/v1/employees/{id}", employeeId));
-
-        //then - verify the result
-        response.andDo(print())
-                .andExpect(status().isOk());
-    }
-
-    //Junit test for delete employee in Negative scenario
-    @DisplayName("Junit test for delete employee in Negative scenario")
-    @Test
-    public void givenEmployeeId_whenDeleteEmployee_thenReturnNotFound() throws Exception {
-
-        //given - is a precondition or a setup
-        long employeeId = 1L;
-        Employee savedEmployee = Employee.builder()
-                .firstName("Sergio")
-                .lastName("Ruy")
-                .emailId("sergio@gmail.com")
-                .build();
-        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.empty());
-        willDoNothing().given(employeeService).deleteEmployee(employeeId); //will do nothing because the method is void
-
-        //when - is the action or the behavior we are going to test
-        ResultActions response = mockMvc.perform(delete("/api/v1/employees/{id}", employeeId));
-
-        //then - verify the result
-        response.andDo(print())
-                .andExpect(status().isNotFound());
     }
 }
