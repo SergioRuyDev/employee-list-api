3be181d754cbe964f018ca33704bf82b49169d14
sergioruydev <sergioruyenator@gmail.com>
2023-03-21 09:28:20 -0300

    Fixed some test.

--- src/main/java/sergioruy/employeelistapi/dto/EmployeeRequestDTO.java
+++ src/main/java/sergioruy/employeelistapi/dto/EmployeeRequestDTO.java
@@ -1,6 +1,11 @@
 package sergioruy.employeelistapi.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
